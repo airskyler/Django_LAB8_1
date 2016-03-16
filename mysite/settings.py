77563d06f96f3db1431eab5f6aeeb91425ee629a
airskyler <su3344qo@go.minneapolis.edu>
2016-03-15 22:53:54 -0500

    Update settings.py
    adding blog message

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
- 'blog',]
+ 'blog',]                              # Adding blog
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
